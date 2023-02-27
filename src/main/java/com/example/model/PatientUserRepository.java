eaad1ddfa178b78838d2053ca758f47075cdaebb
Mariana <marii.teoc@gmail.com>
2023-02-27 11:03:32 -0800

    patient structure

---
+++ src/main/java/com/example/model/PatientUserRepository.java
@@ -0,0 +1,15 @@
+package com.example.model;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PatientUserRepository extends JpaRepository<PatientUserModel,Long>{
+	
+	List<PatientUserModel> findByCard(String healthCard);
+	List<PatientUserModel> findByEmail(String email);
+	Optional<PatientUserModel> findByPatientId(Long patientId);
+	
+
+}
